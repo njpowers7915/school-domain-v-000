c3c723423d1cc8fc1c56f928b244c8ff69037e41
Nicholas Powers <njpowers7915@gmail.com>
2018-05-09 01:04:21 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,12 +9,11 @@ class School
   end
   
   def add_student(student_name, grade)
-    if GRADES_USED.include?(grade) == true
-      @roster[grade] << student_name
-    else
+    if @roster[grade] == nil
       @roster[grade] = []
       @roster[grade] << student_name
-      GRADES_USED << grade
+    else
+      @roster[grade] << student_name
     end
   end
     
